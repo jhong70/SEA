eae382134317e782a689ca74ef17afd2d5f91053
Joon Ki Hong <jhong70@gatech.edu>
2013-06-29 18:40:09 -0400

    Added comments for scripts.js and style improvements

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,58 @@
 //JavaScript Document
 
+//global variables
+var event_boiler_plate;
+var map;
+var userLocLayer;
+var eventsLayer;
+var geocoder;
+var userLatLng;
+var sponsored_results;
+var user_results;
+
 $(document).ready(function() {
-	var event_boiler_plate;
-	$.get('js/event_plate.txt', function(data){
-		event_boiler_plate = data;
-	});
-	//Initialize map and geocoder 
+
+	//MAP AND USER LOCATION INITIALIZATION
 	map = L.mapbox.map('map', 'examples.map-vyofok3q', { zoomControl: false }).setView([40, -74.50], 9);
 	userLocLayer = L.geoJson().addTo(map);
 	eventsLayer = L.geoJson().addTo(map);
 	geocoder = L.mapbox.geocoder('examples.map-vyofok3q');
 	new L.Control.Zoom({ position: 'topright' }).addTo(map);
-	
-	//Ask for users' location
+
+	//---Ask for users' location
 	if (!navigator.geolocation) {
 		alert('geolocation is not available');
 	} else {
 		map.locate({setView:true});
 	}
-	//On location found move and focus the map on users' location
-	latlng = "";
+	//---On location found move and focus the map on users' location
+	userLatLng = "";
 	map.on('locationfound', function(e) {
 		map.fitBounds(e.bounds).setZoom(15);
-		latlng = e.latlng.lat+","+e.latlng.lng;
+		userLatLng = e.latlng.lat+","+e.latlng.lng;
 		//Add a marker on users' location
 		var userIcon = L.icon({
 			iconUrl: 'http://www.prism.gatech.edu/~jhong70/sea/img/markers/user-marker.png',
 			iconAnchor: [12,41]
 		});
-		//Create and bind popup
+		//---Create and bind popup
 		var popupContent = "Here you are!";
 		L.marker([e.latlng.lat, e.latlng.lng], {icon: userIcon}).bindPopup(popupContent,{offset: new L.Point(0,-15)}).addTo(map).openPopup();
 	});
-	
+	//---On location error, alert the user
 	map.on('locationerror', function(e) {
 		console.log(e);
 		alert('Could not track your location');
 	});
-	
-	//Upon pressing enter while search bar is focused, do a search and move the map accordingly
+	//END MAP AND USER LOCATION INITIALIZATION
+
+
+	//SEARCH FUNCTION
+	//---We load the quick event html for sponsored events
+	$.get('js/sponsor_event.txt', function(data){
+		event_boiler_plate = data;
+	});
+	//---Upon pressing enter while search bar is focused, do a search and move the map accordingly
 	$('#searchInput').keypress(function(e) {
 		if(event.keyCode == 13){
 			e.preventDefault();
@@ -46,10 +60,12 @@ $(document).ready(function() {
 				$('#searchInput').blur();
 				$( "#panel" ).panel( "close" );
 				map.setZoom(12);
+				//Clear any event markers from previous searches
 				eventsLayer.clearLayers();
+				//---Eventful query parameters
 				var oArgs = { app_key: "sj98RZjS2GJJGhhH", keywords: $('#searchInput').val(), page_size: 200, location:latlng, within:5}; 
-				EVDB.API.call("/events/search", oArgs, function(oData) {
-					
+				//---Make an Eventful API call and process the data in the callback function
+				EVDB.API.call("/events/search", oArgs, function(oData) {					
 					console.log(oData.total_items);
 					if(oData.total_items<200)
 					for (i=0;i<((oData.total_items<200) ? oData.total_items : 200);i++){
@@ -69,24 +85,24 @@ $(document).ready(function() {
 											   "<strong>Address:</strong> "+venue_address+"</br>";
 							var eventButton;
 							
-							//I have to pass in the current venue_address, event_title, etc. into a function so that the popups don't all share the same values for those variables and instead we get deep copies. Some OP stuff right here...
+							//---I have to pass in the current venue_address, event_title, etc. into a function so that the popups don't all share the same values for those variables and instead we get deep copies. Some OP stuff right here...
 							(function(title,address,desc,s_time,e_time,c_count){
 							eventButton = $("<button type='button' data-role='button'>Event Page</button>").click(function(e){
 									$('#panel').panel('open');
 									$('#panel-result').empty();
 									$('#panel-result').append(event_boiler_plate);
 									//$('#panel-result').append('<p><strong>Event</strong>: '+title+'</p><p><strong>Address</strong>: '+address+'</p><p><strong>Description</strong>: '+desc+'</p><p><strong>Starting time</strong>: '+s_time+'</p><p><strong>Ending time:</strong>: '+e_time+'</p><p><strong>Calendar count</strong>: '+c_count+'</p><button data-role="button">Check in</button>');
-									//Update the panel so that the event info can appear
+									//---Update the panel so that the event info can appear
 									$('#event_title').html(title);
 									$('#event_description').html(desc);
 									$( "#panel" ).trigger( "updatelayout" );
-									//Re-initialize the panel-result div so that jQuery mobile can apply its styling for the contents within
+									//---Re-initialize the panel-result div so that jQuery mobile can apply its styling for the contents within
 									$('#panel-result').trigger("create");
 								})[0];
 							
 							var div = $('<div />').html(eventDesc).append(eventButton)[0];
 							L.marker([oData.events.event[i].latitude, oData.events.event[i].longitude], {icon: evIcon}).bindPopup(div,{offset: new L.Point(0,-15)}).addTo(eventsLayer);
-							
+
 							})(event_title,venue_address,description,start_time,end_time,cal_count);
 							
 						}catch(e){
@@ -118,18 +134,21 @@ $(document).ready(function() {
 			}
 		}
 	});
+	//END SEARCH
 	
+	//QUICK EVENT CLOSE BUTTON FUNCTION
 	$('#panel-close-button').click(function(e) {
 		$( "#panel" ).panel("close");
 	});
 	
+	//PANEL HEIGHT ADJUSTMENTS
 	$('#panel-result').height($(window).height()-$('#header').outerHeight()-$('#panel-close-button').outerHeight()-20);
 	$('#nav-panel').height($(window).height());
 	$('#nav-panel-contents').height($(window).height()-20);
 	
 	
-	//jQM and Leaflet do not play well... This resizes the map to display correctly 
-	if ( map ) // if started
+	//JQM AND LEAFLET (MAP) DO NOT PLAY WELL. MAKE HEIGHT ADJUSTMENTS
+	if ( map ) //---if initialized
     {
     	if($('#map').is(':visible') ) { //just security
         	$('#map').height( $(window).height()-$('#header').outerHeight()-$('#footer').outerHeight() - $('#top-nav').outerHeight() ); 
@@ -138,7 +157,8 @@ $(document).ready(function() {
     	}
     }
 	
-	//We have to readjust the size when the user resizes the window.
+
+	//BIND PANEL AND MAP RESIZE FUNCTIONS
 	$(window).resize(function() {
 		$('#nav-panel-contents').height($(window).height()-20);
 		$('#nav-panel').height($(window).height());
